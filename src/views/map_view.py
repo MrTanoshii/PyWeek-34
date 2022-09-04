e6d2b110977ed103ff8c7f4f38d88e6cc330874c
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-09-05 02:26:09 +0400

    feat: init python arcade
    Co-authored-by: Rohail Malik <gamersassin@gmail.com>

---
+++ src/views/map_view.py
@@ -0,0 +1,61 @@
+from random import random
+import arcade
+import src.const as C
+
+from src.classes import Gold, Research
+
+
+class MapView(arcade.View):
+    """
+    Game View
+
+    ...
+
+    Methods
+    -------
+    on_show()
+        Show the main menu
+    on_draw()
+        Draw the main menu
+    on_mouse_press(x: float, y: float, button: int, modifiers: int)
+        Listen to mouse press event
+    """
+
+    def __init__(self):
+        # Inherit parent class
+        super().__init__()
+
+        # TODO: To remove this. It was for testing only.
+        self.gold = Gold()
+        self.research = Research()
+
+    def on_show(self):
+        """Called when switching to this view."""
+        arcade.set_background_color(C.VIEWS.BACKGROUND_COLOR)
+
+    def on_draw(self):
+        """Draw the map view."""
+
+        # TODO: To remove this. It was for testing only.
+        self.clear()
+        arcade.draw_text(
+            f"Gold {self.gold.value_current} Research {self.research.value_current}",
+            arcade.get_window().width / 2,
+            arcade.get_window().height / 2,
+            arcade.color.BLACK,
+            font_size=C.VIEWS.FONT_SIZE
+            * 2
+            * arcade.get_window().height
+            / C.SETTINGS.SCREEN_HEIGHT,
+            anchor_x="center",
+        )
+
+    def on_update(self, delta_time: float):
+        # TODO: To remove this. It was for testing only.
+        self.gold.increment(1)
+        self.research.increment((random() * 10) - (random() * 10))
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """Use a mouse press to advance to the 'game' view."""
+        # save_data.GameData.read_data()
+        self.window.show_view(MapView())
