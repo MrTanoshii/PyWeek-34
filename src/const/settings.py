23cc625f7d028549d611faeab0e11cf8d6bb5054
Rohail Malik <gamersassin@gmail.com>
2022-09-05 15:39:44 +0400

    Basic game setup, currency & gamedata (#11)
    * feat: init python arcade
    
    Co-authored-by: Rohail Malik <gamersassin@gmail.com>
    
    * feat: add resource interface, gold and research classes
    
    * docs: add game run command
    
    * refactor: remove interface, convert to static
    
    Co-authored-by: jamie nkp <jamienkp@users.noreply.github.com>
    Co-authored-by: Rohail Malik <gamersassin@gmail.com>
    
    * feat: add game data saving & loading
    
    Co-authored-by: Rohail Malik <gamersassin@gmail.com>
    Co-authored-by: jamie nkp <>
    
    Co-authored-by: MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
    Co-authored-by: jamie nkp <jamienkp@users.noreply.github.com>

---
+++ src/const/settings.py
@@ -0,0 +1,10 @@
+class SETTINGS:
+    # Window Config
+    CURSOR_VISIBLE = True
+    CENTER_WINDOW = True
+    FULLSCREEN = False
+    FULLSCREEN_WINDOWED = False
+    RESIZEABLE = True
+    SCREEN_WIDTH = 1280
+    SCREEN_HEIGHT = 720
+    SCREEN_TITLE = "Quack TD by Turtle Duck lTD."
--- src/main.py
+++ src/main.py
@@ -1,2 +1,32 @@
-def hello_world():
-    print("hello  world")
+import arcade
+import pyglet
+
+import src.const as C
+
+from src.views.map_view import MapView
+
+
+def main():
+    """Main function."""
+
+    window = arcade.Window(
+        width=C.SETTINGS.SCREEN_WIDTH,
+        height=C.SETTINGS.SCREEN_HEIGHT,
+        title=C.SETTINGS.SCREEN_TITLE,
+        center_window=C.SETTINGS.CENTER_WINDOW,
+        resizable=C.SETTINGS.RESIZEABLE,
+        fullscreen=C.SETTINGS.FULLSCREEN,
+        style="borderless" if C.SETTINGS.FULLSCREEN_WINDOWED else None,
+    )
+
+    window.set_mouse_visible(C.SETTINGS.CURSOR_VISIBLE)
+    # icon = pyglet.image.load("src/resources/images/cursor.png")
+    # window.set_icon(icon)
+
+    map_view = MapView()
+    window.show_view(map_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
