d589f235c18b7dda0666be306b54e876ff545708
Krzysztof <irateboolean@gmx.com>
2022-09-08 10:11:01 -0400

    added delta time to movement, updated speeds

--- src/enemy/enemy_handler.py
+++ src/enemy/enemy_handler.py
@@ -34,4 +34,6 @@ class EnemyHandler:
         self.enemy_list.draw()
 
     def on_update(self, delta_time: float):
-        self.enemy_list.update()
+        for enemy in self.enemy_list:
+            enemy.update(delta_time)
+
