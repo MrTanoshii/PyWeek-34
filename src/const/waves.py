2f4d0a76bc9d0fd3e9736e2fee0942051ed4fa1e
Krzysztof <irateboolean@gmx.com>
2022-09-09 18:41:44 -0400

    fixed waves

--- src/const/waves.py
+++ src/const/waves.py
@@ -1,5 +1,12 @@
-from src.enemy.enemies import *  # fuck this
+from src.enemy.enemies import enemies as e
 
-WAVES = [
-    [[infantry, 10]],
-]  # TODO: design waves
+
+
+class Waves:
+
+    @staticmethod
+    def wave_1_1():
+        return [[e[6]]*24+[e[0]], 14]
+    @staticmethod
+    def wave_1_2():
+        return [[e[5],e[6],e[1],e[2],e[4],], 7]
