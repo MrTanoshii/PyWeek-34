64a8ef6f30cb888408000d67e6b5d44499a0808e
Krzysztof <irateboolean@gmx.com>
2022-09-09 18:43:27 -0400

    changed waves

--- src/views/map.py
+++ src/views/map.py
@@ -12,6 +12,7 @@ from src.towers import *
 from src.world import *
 
 
+
 class MapView(arcade.View):
     """
     Game View
@@ -191,7 +192,7 @@ class MapView(arcade.View):
             else:
                 self.bgm_player = Audio.play_random(["bgm_1", "bgm_2"])
         elif symbol == arcade.key.S:
-            self.enemy_handler.send_wave(C.WAVES[0].copy(), 5)  # TODO: change this
+            self.enemy_handler.send_wave(*C.Waves.wave_1_1())  # TODO: change this
 
     def remove_tower(self, row: int, column: int):
         self.grid.grid[row][column]["tower"] = None
