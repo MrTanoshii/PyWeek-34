2a43d24d27144e685736925cd8bce31de6bdb96f
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-09-10 02:59:38 +0400

    style: black format

--- src/const/waves.py
+++ src/const/waves.py
@@ -1,12 +1,20 @@
 from src.enemy.enemies import enemies as e
 
 
-
 class Waves:
-
     @staticmethod
     def wave_1_1():
-        return [[e[6]]*24+[e[0]], 14]
+        return [[e[6]] * 24 + [e[0]], 14]
+
     @staticmethod
     def wave_1_2():
-        return [[e[5],e[6],e[1],e[2],e[4],], 7]
+        return [
+            [
+                e[5],
+                e[6],
+                e[1],
+                e[2],
+                e[4],
+            ],
+            7,
+        ]
