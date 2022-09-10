ab8cb5f59ff49c008d4bad3fd18543304e6be1ff
Jeb <jesperkauppinen@gmail.com>
2022-09-11 01:42:22 +0300

    black formatting
    Took 1 hour 7 minutes

--- src/const/waves.py
+++ src/const/waves.py
@@ -3,21 +3,21 @@ from src.enemy.enemies import mech_duck as m
 
 
 def add_mech_duck(
-        _0: int=0,
-        _1: int=0,
-        _2: int=0,
-        _3: int=0,
-        _4: int=0,
-        _5: int=0,
-        _6: int=0,
-        _7: int=0,
-        _8: int=0,
-        _9: int=0,
-        _10: int=0,
-        _11: int=0,
-        _12: int=0,
-        _13: int=0,
-        _14: int=0,
+    _0: int = 0,
+    _1: int = 0,
+    _2: int = 0,
+    _3: int = 0,
+    _4: int = 0,
+    _5: int = 0,
+    _6: int = 0,
+    _7: int = 0,
+    _8: int = 0,
+    _9: int = 0,
+    _10: int = 0,
+    _11: int = 0,
+    _12: int = 0,
+    _13: int = 0,
+    _14: int = 0,
 ):
     wave = []
     wave = wave + [m[0]] * _0
@@ -38,17 +38,18 @@ def add_mech_duck(
 
     return wave
 
+
 def add_enemy(
-        _0: int=0,
-        _1: int=0,
-        _2: int=0,
-        _3: int=0,
-        _4: int=0,
-        _5: int=0,
-        _6: int=0,
-        _7: int=0,
-        _8: int=0,
-        _9: int=0,
+    _0: int = 0,
+    _1: int = 0,
+    _2: int = 0,
+    _3: int = 0,
+    _4: int = 0,
+    _5: int = 0,
+    _6: int = 0,
+    _7: int = 0,
+    _8: int = 0,
+    _9: int = 0,
 ):
     wave = []
     wave = wave + [e[0]] * _0
@@ -62,13 +63,10 @@ def add_enemy(
     wave = wave + [e[8]] * _8
     wave = wave + [e[9]] * _9
 
-
     return wave
 
 
 class Waves:
-
-
     @staticmethod
     def wave_1_1():
         return [[e[6]] * 24 + [e[0]], 14]
@@ -97,26 +95,26 @@ class Waves:
     @classmethod
     def level_1(cls, wave: int):
         if wave == 1:
-            _wave = add_enemy(20,25,22,45)
+            _wave = add_enemy(20, 25, 22, 45)
         elif wave == 2:
-            _wave = add_enemy(0,0,0,20,25,22,45)
+            _wave = add_enemy(0, 0, 0, 20, 25, 22, 45)
         elif wave == 3:
-            _wave = add_enemy(0,0,20,25,22,45)
+            _wave = add_enemy(0, 0, 20, 25, 22, 45)
         elif wave == 4:
-            _wave = add_enemy(0,20,25,22,450,0,0,0,0,12)
+            _wave = add_enemy(0, 20, 25, 22, 450, 0, 0, 0, 0, 12)
         elif wave == 5:
-            _wave = add_enemy(20,25,22,45,0,0,120,100,75,22)
+            _wave = add_enemy(20, 25, 22, 45, 0, 0, 120, 100, 75, 22)
         elif wave == 6:
-            _wave = add_enemy(20,25,22,450,44,22,13,8,7,2)
+            _wave = add_enemy(20, 25, 22, 450, 44, 22, 13, 8, 7, 2)
         elif wave == 7:
-            _wave = add_enemy(20,25,22,45,0,41,55,74,12,13)
+            _wave = add_enemy(20, 25, 22, 45, 0, 41, 55, 74, 12, 13)
         elif wave == 8:
-            _wave = add_enemy(20,25,22,45,11,45,68,78,123)
+            _wave = add_enemy(20, 25, 22, 45, 11, 45, 68, 78, 123)
         elif wave == 9:
-            _wave = add_enemy(20,25,22,45,12,123,14,159,753,656)
+            _wave = add_enemy(20, 25, 22, 45, 12, 123, 14, 159, 753, 656)
         else:
             return None
-        return [_wave, len(_wave)//wave]
+        return [_wave, len(_wave) // wave]
 
     @classmethod
     def level(cls, map: str, wave: int):
@@ -125,26 +123,27 @@ class Waves:
             return cls.level_duck(wave)
         else:
             return cls.level_1(wave)
+
     @classmethod
     def level_duck(cls, wave: int):
         if wave == 1:
-            ducks = add_mech_duck(20,25,22,45)
+            ducks = add_mech_duck(20, 25, 22, 45)
         elif wave == 2:
-            ducks = add_mech_duck(0,0,0,20,25,22,45)
+            ducks = add_mech_duck(0, 0, 0, 20, 25, 22, 45)
         elif wave == 3:
-            ducks = add_mech_duck(0,0,0,0,0,0,0,0,20,25,22,45)
+            ducks = add_mech_duck(0, 0, 0, 0, 0, 0, 0, 0, 20, 25, 22, 45)
         elif wave == 4:
-            ducks = add_mech_duck(0,0,0,0,20,25,22,450,0,0,0,0,12)
+            ducks = add_mech_duck(0, 0, 0, 0, 20, 25, 22, 450, 0, 0, 0, 0, 12)
         elif wave == 5:
-            ducks = add_mech_duck(20,25,22,45,0,0,0,120,100,75,22)
+            ducks = add_mech_duck(20, 25, 22, 45, 0, 0, 0, 120, 100, 75, 22)
         elif wave == 6:
-            ducks = add_mech_duck(20,25,22,45,0,0,0,0,0,44,22,13,8,7,2)
+            ducks = add_mech_duck(20, 25, 22, 45, 0, 0, 0, 0, 0, 44, 22, 13, 8, 7, 2)
         elif wave == 7:
-            ducks = add_mech_duck(20,25,22,45,0,41,55,74,12,13,5)
+            ducks = add_mech_duck(20, 25, 22, 45, 0, 41, 55, 74, 12, 13, 5)
         elif wave == 8:
-            ducks = add_mech_duck(20,25,22,45,11,45,68,78,123)
+            ducks = add_mech_duck(20, 25, 22, 45, 11, 45, 68, 78, 123)
         elif wave == 9:
-            ducks = add_mech_duck(20,25,22,45,12,123,14,159,753,656)
+            ducks = add_mech_duck(20, 25, 22, 45, 12, 123, 14, 159, 753, 656)
         else:
             return None
-        return [ducks, len(ducks)//wave]
+        return [ducks, len(ducks) // wave]
