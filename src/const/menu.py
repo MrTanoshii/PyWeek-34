3306b3036f6b45509b81811e5c128155cb74e840
Jeb <jesperkauppinen@gmail.com>
2022-09-10 22:52:25 +0300

    level menu
    Took 27 minutes

--- src/const/menu.py
+++ src/const/menu.py
@@ -2,17 +2,23 @@ from pathlib import Path
 
 
 class MENU:
-    PREVIEW_BASEPATH = Path("src") / "world" / "previews"
+    PREVIEW_BASEPATH = Path("src") / "gui" / "sprites"
     LEVELS = {
-        "level 1 name": "draft_level_1",  # name of map and its preview (e.g. draft_level_1.json and draft_level_1.png)
-        "level 2 name": "draft_level_2",
-        "level 3 name": "draft_level_1",
-        "level 4 name": "draft_level_1",
-        "level 5 name": "draft_level_1",
-        "level 6 name": "draft_level_1",
-        "level 7 name": "draft_level_1",
-        "level 8 name": "draft_level_1",
-    }  # TODO
-    SECRET_LEVELS = ["draft_level_3"]  # TODO
+        # name of map and its preview (e.g. draft_level_1.json and draft_level_1.png)
+        "Duck level": "draft_level_secret_duck",
+        "Penguin level": "draft_level_secret_penguin",
+        "Turtle level": "draft_level_secret_turtle",
+        "level 1": "draft_level_1",
+        "level 2": "draft_level_2",
+        "level 3": "draft_level_3",
+        "level 4": "draft_level_4",
+        "level 5": "draft_level_5",
+    }
+    # SECRET_LEVELS = [
+    #     "draft_level_secret_duck",
+    #     "draft_level_secret_penguin",
+    #     "draft_level_secret_turtle",
+    # ]
+    SECRET_LEVELS = []
     LEVELS_IN_ROW = 3
     HOVER_COLOR = (0, 0, 0, 48)
